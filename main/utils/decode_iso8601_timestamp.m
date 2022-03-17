3ffcc78a9e5bc0634e295ea0b5ca0c54fc3dd2f1
Vasileios Papapanagiotou <vassilis@mug.ee.auth.gr>
2022-03-17 16:50:04 +0200

    updates for ISO8601

--- main/utils/decode_iso8601_timestamp.m
+++ main/utils/decode_iso8601_timestamp.m
@@ -1,35 +1,81 @@
-function [utc, lcl] = decode_iso8601_timestamp(ts)
+function [utc, lcl] = decode_iso8601_timestamp(t)
     %DECODE_ISO8601_TIMESTAMP Decodes a timestamp to UTC and local-time MATLAB timestamps
     %
-    %   [utc, lcl] = DECODE_ISO8601_TIMESTAMP(ts) decodes an ISO8601-formatted
-    %   string into UTC and local-time timestamps (using MATLAB representation).
+    %   [utc, lcl] = DECODE_ISO8601_TIMESTAMP(t) decodes an
+    %   ISO8601-formatted string into UTC and local-time timestamps (using
+    %   MATLAB representation). 
     
-    if isempty(ts)
+    if isempty(t)
         utc = [];
         lcl = [];
         return
     end
     
-    if iscell(ts)
-        ts = ts{1};
+    if iscell(t)
+        if numel(t) == 1
+            t = t{1};
+        else
+            error('Cannot handle cell arrays')
+        end
     end
     
-    s1 = ts(1:19);  % main timestamp (no millis)
-    s2 = ts(end - 5:end);  % timezone
-    if length(ts) > 25
-        s3 = ts(20 + (1:length(ts) - 26));  % millis
-    else
-        s3 = '0';
-    end
-
-    % Timezone handling
-    s2a = str2double([s2(1) '1']);
-    s2b = str2double(s2(2:3));
-    s2c = str2double(s2(5:6));
-    tz = s2a * datenum(0, 0, 0, s2b, s2c, 0);
+    t = remove_locale(t);
     
-    lcl = datenum(s1, 'yyyy-mm-ddTHH:MM:SS');
-    lcl = lcl + in_days(str2double(s3), 'millis');
+    [dt, tz] = split_datetime_timezone(t);
 
+    lcl = iso8601_to_datenum(dt);
+    tz = timezone_in_days(tz);  % tz is not ISO8601, but the code is the same
+    
     utc = lcl - tz;
+end
+
+
+function t = remove_locale(t)
+    % Remove locale if it exists
+    idx = strfind(t, '[');
+    if length(idx) == 1
+        t = t(1:idx - 1);
+    elseif length(idx) > 1
+        error(['Cannot handle ''', t, ''''])
+    end
+end
+
+
+function [dt, tz] = split_datetime_timezone(t)
+    
+    % Handle 'Z' first, to avoid problems
+    if strcmp(t(end), 'Z')
+        dt = t(1:end - 1);
+        tz = '+00:00';
+        return
+    end
+    
+    % Find timezone start using '+' or '-'   
+    idx1 = strfind(t(end - 5:end), '+');
+    idx2 = strfind(t(end - 5:end), '-');
+
+    % There should only be only one '+' or '-'
+    if length(idx1) + length(idx2) ~= 1
+        error(['Cannot handle ''', t, ''''])
+    end
+
+    % Get index of split
+    idx = length(t) - 6 + [idx1, idx2];
+    
+    dt = t(1:idx - 1);
+    tz = t(idx:end);
+end
+
+
+function dt = iso8601_to_datenum(dt)
+    dt(strfind(dt, 'T')) = ' ';
+    dt = datenum(dt);
+end
+
+
+function a = timezone_in_days(tz)
+    hours = str2double(tz(1:3));
+    minutes = str2double(tz(end - 1:end));
+    
+    a = (hours + minutes / 60) / 24;
 end
--- main/utils/encode_iso8601_timestamp.m
+++ main/utils/encode_iso8601_timestamp.m
@@ -1,9 +1,24 @@
-function s = encode_iso8601_timestamp(t)
+function timestamp = encode_iso8601_timestamp(t, tz_sec)
     %ENCODE_ISO8601_TIMESTAMP Encodes (formats) a timestamp to ISO8601
     %
-    %   s = DECODE_ISO8601_TIMESTAMP(t) formats a timestamp to ISO8601
-    %   specification. Since MATLAB timestamps are time-zone--agnostic,
-    %   timezone is set to '+00:00'.
+    %   timestamp = DECODE_ISO8601_TIMESTAMP(t) formats a timestamp to
+    %   ISO8601 specification. Since MATLAB timestamps are
+    %   time-zone--agnostic, timezone is set to '+00:00'.
+    %
+    %   timestamp = DECODE_ISO8601_TIMESTAMP(t, tz_sec) allows to specify
+    %   the timezone offest (in seconds).
     
-    s = datestr(t, 'yyyy-mm-ddTHH:MM:SS.FFF+00:00');
+    
+    if nargin < 2
+        tz = '+00:00';
+    else
+        tz_days = in_days(tz_sec, 'sec');
+        t = t + tz_days;
+        
+        s = repmat('+', size(t));
+        s(tz_sec < 0) = '-';
+        tz = [s, datestr(abs(tz_days), 'HH:MM')];
+    end
+    
+    timestamp = [datestr(t, 'yyyy-mm-ddTHH:MM:SS.FFF'), tz];
 end
