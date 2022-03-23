3c9968cc400a46aac002fda7785a106144bf2d0b
Vasileios Papapanagiotou <vassilis@mug.ee.auth.gr>
2022-03-23 08:53:44 +0200

    interp1gaps

--- main/dsp/interp1gaps.m
+++ main/dsp/interp1gaps.m
@@ -3,6 +3,7 @@ function [t, X] = interp1gaps(t, X, target_fs, min_gap, method)
     %
     %   [t, X] = interp1gaps(t, X, target_fs, min_gap) interpolates when
     %   there are gaps larger than min_gap seconds in the timestamps t.
+    %
     %   [t, X] = interp1gaps(t, X, target_fs, min_gap, method) specifies
     %   the interpolation method. See INTERP1 for possible options.
     
@@ -20,13 +21,17 @@ function [t, X] = interp1gaps(t, X, target_fs, min_gap, method)
     idx = find(diff(t) >= min_gap);
     
     for i = 1:length(idx)
+        idx1 = idx(i);
+        idx2 = idx(i) + 1;
         % Interpolate
-        t_q = t(idx(i)) + dt:dt:t(idx(i) + 1) - T;
+        t_q = t(idx1) + dt:dt:t(idx2) - dt;
+        t_q = t_q';
+        X_q = f(t(idx1:idx2), X(idx1:idx2, :), t_q);
+        
         % X_q = repmat(X(idx(i), :), [length(t_q), 1]);
-        X_q = f(t(idx(i):idx(i) + 1), X(idx(i):idx(i) + 1, :), t_q);
         
         % Append
-        t = [t; t_q'];
+        t = [t; t_q];
         X = [X; X_q];
     end
     
